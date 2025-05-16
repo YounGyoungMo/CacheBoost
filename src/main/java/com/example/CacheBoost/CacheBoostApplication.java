f9cbd19c6c17ddf8fa98b40635ab74d0ed01eac1
Chaemin07 <gkc05075@gmail.com>
2025-05-16 19:59:31 +0900

    feat: 프로젝트 초기 설정 및 핵심 의존성 추가
    - Spring Web, Spring Data JPA, Lombok, MySQL Driver 의존성 추가
    - Spring Initializr 기반 프로젝트 생성

---
+++ src/main/java/com/example/CacheBoost/CacheBoostApplication.java
@@ -0,0 +1,13 @@
+package com.example.CacheBoost;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CacheBoostApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CacheBoostApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/CacheBoost/CacheBoostApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CacheBoost;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CacheBoostApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
