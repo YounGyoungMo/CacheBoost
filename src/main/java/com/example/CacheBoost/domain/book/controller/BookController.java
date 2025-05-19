3ef2e245e7bfe326c5d5ffa7303ee2f6d572b044
HanGil <jhg9240@gmail.com>
2025-05-19 10:11:21 +0900

    임시 커밋: 수정사항 임시 저장

--- src/main/java/com/example/CacheBoost/domain/book/controller/BookController.java
+++ src/main/java/com/example/CacheBoost/domain/book/controller/BookController.java
@@ -1,4 +1,24 @@
 package com.example.CacheBoost.domain.book.controller;
 
+import com.example.CacheBoost.domain.book.dto.RequestDto.AddBookRequestDto;
+import com.example.CacheBoost.domain.book.dto.ResponseDto.BookResponseDto;
+import com.example.CacheBoost.domain.book.service.BookService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping
 public class BookController {
+
+    private final BookService bookService;
+
+    @PostMapping("/admin/books")
+    public ResponseEntity<BookResponseDto> addBook(@RequestBody AddBookRequestDto requestDto) {
+        BookResponseDto bookResponseDto = bookService.addBook(requestDto);
+    }
 }
