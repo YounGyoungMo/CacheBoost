b62201e00bc1a97d0773762efc27d657b8ffd284
Chaemin07 <gkc05075@gmail.com>
2025-05-19 18:59:59 +0900

    feat: User 도메인의 기본 Controller, Service, Repository 생성
    - 사용자 관련 기본 구조 생성

--- src/main/java/com/example/CacheBoost/domain/user/repository/UserRepository.java
+++ src/main/java/com/example/CacheBoost/domain/user/repository/UserRepository.java
@@ -1,4 +1,10 @@
 package com.example.CacheBoost.domain.user.repository;
 
-public interface UserRepository {
+import com.example.CacheBoost.domain.user.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
 }
