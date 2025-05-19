46e4ce7e051d090b7238a4a69bb8183c8b570e3e
Chaemin07 <gkc05075@gmail.com>
2025-05-19 10:18:37 +0900

    feat: User 엔티티, Role enum 및 BaseEntity 생성
    - 사용자 정보 관리를 위한 User 엔티티 정의
    - 사용자 권한 구분을 위한 Role enum 추가
    - 생성일, 수정일 관리를 위한 BaseEntity 생성

--- src/main/java/com/example/CacheBoost/common/entity/BaseTimeEntity.java
+++ src/main/java/com/example/CacheBoost/common/entity/BaseTimeEntity.java
@@ -1,4 +1,27 @@
 package com.example.CacheBoost.common.entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
 public class BaseTimeEntity {
+
+    // 생성 시간
+    @CreatedDate
+    @Column(updatable = false)
+    private LocalDateTime createdAt;
+
+    // 수정 시간
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
 }
