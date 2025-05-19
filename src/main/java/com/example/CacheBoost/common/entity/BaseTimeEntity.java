31b73e766c9321c374de122086222ee76ff77eb7
YounGyoungMo <gyoungmo2011@naver.com>
2025-05-19 09:52:26 +0900

    refactor : BaseTimeEntity 수정

--- src/main/java/com/example/CacheBoost/common/entity/BaseTimeEntity.java
+++ src/main/java/com/example/CacheBoost/common/entity/BaseTimeEntity.java
@@ -1,5 +1,6 @@
 package com.example.CacheBoost.common.entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
 import org.springframework.data.annotation.CreatedDate;
@@ -12,8 +13,10 @@ import java.time.LocalDateTime;
 @EntityListeners(AuditingEntityListener.class)
 public abstract class BaseTimeEntity {
     @CreatedDate
+    @Column(nullable = false, updatable = false)
     private LocalDateTime createdAt;
 
     @LastModifiedDate
+    @Column(nullable = false)
     private LocalDateTime updatedAt;
 }
